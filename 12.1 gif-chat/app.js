570b4a8ea5e3ce7193b03987eea99da3c8441e24
kyjiya <kyjiya@github.io>
2021-05-15 01:02:51 +0900

    20210515_12.5 미들웨어와 소켓 연결하기

--- 12.1 gif-chat/app.js
+++ 12.1 gif-chat/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const ColorHash = require('color-hash');
 
 dotenv.config();
 const webSocket = require('./socket');
@@ -21,11 +22,21 @@ nunjucks.configure('views', {
 });
 connect();
 
+const sessionMiddleware = session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+});
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
 app.use(session({
     resave: false,
     saveUninitialized: false,
@@ -35,6 +46,14 @@ app.use(session({
         secure: false,
     },
 }));
+
+app.use((req, res, next) => {
+    if (!req.session.color) {
+        const colorHash = new ColorHash();
+        req.session.color = colorHash.hex(req.sessionID);
+    }
+    next();
+});
 
 app.use('/', indexRouter);
 
@@ -55,4 +74,4 @@ const server = app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
